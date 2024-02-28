03fd13d6fe003427dada7046bccd34afe3b5a1fe
Vivek Tiwary <vivektiwary@microsoft.com>
2024-02-28 15:52:43 +0530

    LATEST CHANGES

--- ResolveProjectDependency/Program.cs
+++ ResolveProjectDependency/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 internal class Program
 {
@@ -8,17 +9,106 @@ internal class Program
         var projectPath = @"C:\Users\vivektiwary\workspace\TM-Hachathon\todo-csharp-sql";
 
         //read all the folders in projectPath & find files with .csproj extension & package.json
-        var projectFiles = Directory.GetFiles(projectPath, "*.csproj", SearchOption.AllDirectories);
         var packageJsonFiles = Directory.GetFiles(projectPath, "package.json", SearchOption.AllDirectories);
+
+        // read file with .sln extension and return it's absolute path
+
+        var solutionFiles = Directory.GetFiles(projectPath, "*.sln", SearchOption.AllDirectories);
+        if (solutionFiles.Length > 0)
+        {
+            var solutionFile = solutionFiles[0];
+            ApplicationInformation projectDependecies = ComputeCsProjDependencies(solutionFile);
+            string json = JsonConvert.SerializeObject(projectDependecies);
+            Console.WriteLine(json);
+        }
+        else
+        {
+            Console.WriteLine("No solution file found.");
+        }
+
 
         ComputePackageJsonDependencies(packageJsonFiles);
     }
 
 
-    static void ComputeCsProjDependencies(string[] projectFiles)
+    static ApplicationInformation ComputeCsProjDependencies(string solutionFile)
     {
+        var solutionName = Path.GetFileNameWithoutExtension(solutionFile);
+        // Run dotnet restore
+        var restoreProcess = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"restore {solutionFile}",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        restoreProcess.Start();
+        restoreProcess.WaitForExit(); // Wait for the restore process to complete
+
+        Console.WriteLine($"Restored packages for solution: {solutionName}");
+
+        // write C# code to fetch all dependencies from solution file file using dotnet cli via Process class
+
+        var process = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"list {solutionFile} package",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+
+        var packages = new HashSet<string>();
+        bool readPackages = false;
+
+        while (!process.StandardOutput.EndOfStream)
+        {
+            var line = process.StandardOutput.ReadLine();
+
+            // Start reading packages when the line with 'Top-level Package' is encountered
+            if (!readPackages && line.Contains("Top-level Package"))
+            {
+                readPackages = true;
+                continue;
+            }
+
+            // Stop reading packages when an empty line is encountered
+            if (readPackages && string.IsNullOrWhiteSpace(line))
+            {
+                break;
+            }
+
+            if (readPackages)
+            {
+                // Extract the package name from the line
+                var parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var packageName = parts[1];
+                var version = parts[2];
+                packages.Add($"{packageName}, Version: {version}");
+            }
+        }
+
+        packages.RemoveWhere(x => x.StartsWith("System", StringComparison.OrdinalIgnoreCase) || x.StartsWith("Swashbuckle", StringComparison.OrdinalIgnoreCase));
+
+        return new ApplicationInformation()
+        {
+            ProjectType = ProjectType.DotNet,
+            ProjectName = solutionName,
+            AppDependencies = packages.ToList()
+        };
 
     }
+
 
 
     static List<ApplicationInformation> ComputePackageJsonDependencies(string[] packageJsonFiles)
@@ -45,7 +135,7 @@ internal class Program
                         {
                             ProjectType = ProjectType.React,
                             ProjectName = projectName,
-                            AppDependencies = ComputeCurrentPackageDependencies(packageJsonFile)
+                            //AppDependencies = ComputeCurrentPackageDependencies(packageJsonFile)
                         });
                     }
 
@@ -66,7 +156,7 @@ class ApplicationInformation
 {
     public ProjectType ProjectType { get; set; }
     public string ProjectName { get; set; }
-    public List<ApplicationInformation> AppDependencies { get; set; }
+    public List<string> AppDependencies { get; set; }
 
 }
 

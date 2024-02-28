db6843ac5622197189dc693e8370a5c867a4e757
Vivek Tiwary <vivektiwary@microsoft.com>
2024-02-28 11:10:22 +0530

    Add project files.

---
+++ ResolveProjectDependency/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var projectPath = "";
+
+        //read all the folders in projectPath & find files with .csproj extension & package.json
+        var projectFiles = Directory.GetFiles(projectPath, "*.csproj", SearchOption.AllDirectories);
+        var packageJsonFiles = Directory.GetFiles(projectPath, "package.json", SearchOption.AllDirectories);
+        foreach (var projectFile in projectFiles)
+        {
+
+        }
+    }
+
+
+    static void ComputeCsProjDependencies(string projectFile)
+    {
+        
+    }   
+
+
+    static void ComputePackageJsonDependencies(string packageJsonFile)
+    {
+        
+    }
+}

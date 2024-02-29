f478ba0fcd67e74931c20c96d90519850bdd5e7f
Vivek Tiwary <vivektiwary@microsoft.com>
2024-02-29 10:24:48 +0530

    Node depdency check not required

--- ResolveProjectDependency/Utils/AppValidations.cs
+++ ResolveProjectDependency/Utils/AppValidations.cs
@@ -47,21 +47,22 @@ public static class AppValidations
     //method to check node is installed or not
     private static bool IsNodeInstalled()
     {
-        var process = new Process()
-        {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = "node",
-                Arguments = "--version",
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-            }
-        };
+        //var process = new Process()
+        //{
+        //    StartInfo = new ProcessStartInfo
+        //    {
+        //        FileName = "node",
+        //        Arguments = "-v",
+        //        RedirectStandardOutput = true,
+        //        UseShellExecute = false,
+        //        CreateNoWindow = true,
+        //    }
+        //};
 
-        process.Start();
-        var version = process.StandardOutput.ReadToEnd();
-        return !string.IsNullOrWhiteSpace(version);
+        //process.Start();
+        //var version = process.StandardOutput.ReadToEnd();
+        //return !string.IsNullOrWhiteSpace(version);
+        return true;
     }
 
     public static bool InitValidation() => IsDotnetCliInstalled() && IsGitCliInstalled() && IsNodeInstalled();
